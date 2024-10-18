803ffa9ee3ef3e05e2446be3bbfa71cadc19537c
aaronrs2002 <aaronrs2002@yahoo.com>
2024-10-18 12:07:21 -0700

    Update calendar.js
    updated calendar

--- js/calendar.js
+++ js/calendar.js
@@ -1,56 +1,118 @@
 
 /*START CALENDAR*/
 let calendarData = [];
+let taskList = [];
 let dateTxt = null;
 let updateCalendar = true;
 let activePad = "default";
 let whichMonth = "";
 
 
-const renderCalendar = (data) => {
-
-    console.log("JSON.stringify(data): " + JSON.stringify(data));
 
 
+
+
+
+const renderCalendar = (data, from) => {
+
+
+    console.log("render calendar FROM: " + from);
     if (data.length === 0) {
         globalAlert("alert-info", "There is no data for this pad withing this month.");
         return false;
     }
-    [].forEach.call(document.querySelectorAll("[data-direction='0']"), (e) => {
-        let fistTxt = e.innerHTML;
 
-        if (fistTxt.length === 1) {
-            fistTxt = "0" + fistTxt;
-        }
-        e.dataset.daynum = whichMonth + "-" + fistTxt;
+
+    console.log("JSON.stringify(data): " + JSON.stringify(data));
+
+
+    [].forEach.call(document.querySelectorAll("[data-daynum]"), (e) => {
+        //e.innerHTML = "";
+
+        e.innerHTML = '';
+
     });
 
-    for (let i = 0; i < data.length; i++) {
-        let tempStart = data[i].start;
-        let tempEnd = data[i].end;
 
-        [].forEach.call(document.querySelectorAll("[data-daynum]"), (e) => {
-            let dayNames = [];
+    [].forEach.call(document.querySelectorAll("[data-daynum]"), (e) => {
+        e.innerHTML = "";
+        let dayVal = e.getAttribute("data-daynum");
+        let calendarCellHTML = dayVal.substring(8, 10);
+        for (let i = 0; i < data.length; i++) {
+            let tempStart = data[i].start;
+            let tempEnd = data[i].end;
+
+
+            // e.innerHTML = dayVal.substring(8, 10)
+
             let sqDay = e.dataset.daynum.replaceAll("-", "");
             sqDay = parseInt(sqDay);
             let tempStartDyNum = tempStart.replaceAll("-", "");
             tempStartDyNum = parseInt(tempStartDyNum);
             let tempEndDyNum = tempEnd.replaceAll("-", "");
             tempEndDyNum = parseInt(tempEndDyNum);
-            if (Number(sqDay) >= tempStartDyNum && Number(sqDay) <= tempEndDyNum) {
-                let customName = data[i].title;
+            console.log("data[i].title: " + data[i].title);
 
-                if (customName && dayNames.indexOf(customName) === -1) {
-                    e.innerHTML = e.innerHTML + "<span class='badge rounded-pill bg-dark' title='" + customName + "'>  " + customName + "</span>";
+
+
+            if (data[i].title) {
+
+                console.log("calendarCellHTML: " + calendarCellHTML);
+                if (Number(sqDay) >= tempStartDyNum && Number(sqDay) <= tempEndDyNum && calendarCellHTML.indexOf(" title='" + data[i].title + "' ") === -1) {
+                    let customName = data[i].title;
+                    calendarCellHTML = calendarCellHTML + "<span class='badge rounded-pill bg-dark' data-daynum='" + dayVal + "' title='" + customName + "'>  " + customName.substring(0, 2) + "</span>";
+
+                    console.log("data[i].start: " + data[i].start);
+                    console.log("data[i].end: " + data[i].end);
+
+
+
                 }
-                dayNames.push(customName);
-            }
-        });
 
-    }
+
+            }
+
+        }
+
+        e.innerHTML = calendarCellHTML;
+        /*
+    
+ e.innerHTML
+
+      let dayListHTML = "";
+        for (let j = 0; j < dayList.length; j++) {
+
+            if (Number(sqDay) >= tempStartDyNum && Number(sqDay) <= tempEndDyNum) {
+                let customName = dayList[j].title;
+                console.log("document.querySelector(span[data - daynum= ' + dayVal + '][title = ' + customName + ']): " + document.querySelector("span[data-daynum='" + dayVal + "'][title='" + customName + "']"))
+
+                dayListHTML = dayListHTML + "<span class='badge rounded-pill bg-dark' data-daynum='" + dayVal + "' title='" + customName + "'>  " + customName.substring(0, 2) + "</span>";
+
+
+            }
+
+        }
+
+        e.innerHTML = dayListHTML;
+        */
+
+
+    });
+
+
+
+
+
     updateCalendar = false;
+
+
+    return false;
+
 }
-const picker = datepicker('#datePickerCalendarTarget', {
+
+
+
+let picker = datepicker('#datePickerCalendarTarget', {
 
     // Event callbacks.
     onSelect: instance => {
@@ -70,7 +132,14 @@ const picker = datepicker('#datePickerCalendarTarget', {
         }
     },
     onHide: instance => {
-        console.log('Calendar hidden.')
+        console.log('Calendar hidden.');
+
+        [].forEach.call(document.querySelectorAll("[data-daynum]"), (e) => {
+            //e.innerHTML = "";
+
+            let dayVal = e.getAttribute("data-daynum");
+            e.innerHTML = dayVal.substring(8, 10)
+        });
     },
     onMonthChange: instance => {
         // Show the month of the selected date. 
@@ -81,17 +150,27 @@ const picker = datepicker('#datePickerCalendarTarget', {
             tempMonth = "0" + tempMonth;
         }
         whichMonth = instance.currentYear + "-" + tempMonth;
-        let tempData = [];
-        let tempTasKList = JSON.parse(localStorage.getItem("taskList"));
-        for (let i = 0; i < tempTasKList.length; i++) {
-            let endStamp = tempTasKList[i].details.substring(tempTasKList[i].details.indexOf(":") + 7, tempTasKList[i].details.indexOf(":") + 11) + "-" +
-                tempTasKList[i].details.substring(tempTasKList[i].details.indexOf(":") + 1, tempTasKList[i].details.indexOf(":") + 3) + "-" +
-                tempTasKList[i].details.substring(tempTasKList[i].details.indexOf(":") + 4, tempTasKList[i].details.indexOf(":") + 6);
+        /* let tempData = [];
+         let tempTasKList = JSON.parse(localStorage.getItem("taskList"));
+         for (let i = 0; i < tempTasKList.length; i++) {
+             let endStamp = tempTasKList[i].details.substring(tempTasKList[i].details.indexOf(":") + 7, tempTasKList[i].details.indexOf(":") + 11) + "-" +
+                 tempTasKList[i].details.substring(tempTasKList[i].details.indexOf(":") + 1, tempTasKList[i].details.indexOf(":") + 3) + "-" +
+                 tempTasKList[i].details.substring(tempTasKList[i].details.indexOf(":") + 4, tempTasKList[i].details.indexOf(":") + 6);
+ 
+             tempData.push({ title: tempTasKList[i].task, start: timeStamp(), end: endStamp });
+ 
+         }
+         renderCalendar(tempData);*/
 
-            tempData.push({ title: tempTasKList[i].task, start: timeStamp(), end: endStamp });
+        [].forEach.call(document.querySelectorAll("[data-daynum]"), (e) => {
+            //e.innerHTML = "";
 
-        }
-        renderCalendar(tempData);
+            e.innerHTML = '';
+
+        });
+
+        renderCalendar(taskList, "monthChange");
+
     },
     // Customizations.
     formatter: (input, date, instance) => {
@@ -125,5 +204,18 @@ const picker = datepicker('#datePickerCalendarTarget', {
      disableYearOverlay: true, // Clicking the year or month will *not* bring up the year overlay.*/
     // ID - be sure to provide a 2nd picker with the same id to create a daterange pair.
     id: 1
+
+
 });
+
+
+[].forEach.call(document.querySelectorAll("[data-direction='0']"), (e) => {
+    let fistTxt = e.innerHTML;
+
+    if (fistTxt.length === 1) {
+        fistTxt = "0" + fistTxt;
+    }
+    e.dataset.daynum = whichMonth + "-" + fistTxt;
+});
+
 
