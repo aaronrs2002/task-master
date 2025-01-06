75508c9a0d57858897746329ee4560e1f36f7378
Aaron Smith <aaron@web-presence.biz>
2025-01-06 15:29:42 -0700

    adding nav

--- js/sharedFunctionality.js
+++ js/sharedFunctionality.js
@@ -49,3 +49,42 @@ const timeStamp = () => {
     return today;
 
 }
+
+const toggleMobileNav = (whichElem) => {
+
+    if (whichElem === "mobileNav") {
+        if (document.querySelector(".collapse[data-toggle='mobileNav']")) {
+            document.querySelector("[data-toggle='mobileNav']").classList.remove("collapse");
+            document.querySelector("[data-toggle='mobileNav']").classList.add("show");
+        } else {
+            document.querySelector("[data-toggle='mobileNav']").classList.remove("show");
+            document.querySelector("[data-toggle='mobileNav']").classList.add("collapse");
+        }
+
+    }
+
+
+}
+
+/*
+                    <li class="nav-item">
+                        <a class="nav-link active" href="https://aaronrs2002.github.io/time-clock/">Time Clock</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="https://aaronrs2002.github.io/task-master/">Task Master</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="https://aaronrs2002.github.io/analyze-bank-files/">Budget</a>
+                    </li>
+*/
+
+let navData = [{ name: "Clock In Clock Out", address: "https://aaronrs2002.github.io/time-clock/" }, { name: "Task Master", address: "https://aaronrs2002.github.io/task-master/" }, { name: "Analyze bank records", address: "https://aaronrs2002.github.io/analyze-bank-files/" }];
+let navLinkHTML = "";
+for (let i = 0; i < navData.length; i++) {
+    let active = "";
+    if (navData[i].name === document.querySelector("title").innerHTML) {
+        active = "active";
+    }
+    navLinkHTML = navLinkHTML + "<li class='nav-item'> <a class='nav-link " + active + "' href=" + navData[i].address + ">" + navData[i].name + "</a></li>";
+}
+document.getElementById("navLinkTarget").innerHTML = navLinkHTML;
