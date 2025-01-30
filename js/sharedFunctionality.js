ab43657bfbd054d72fce9ebc8f5f20ffacc8db32
Aaron Smith <aaron@web-presence.biz>
2025-01-30 15:46:08 -0700

    added budget to nav

--- js/sharedFunctionality.js
+++ js/sharedFunctionality.js
@@ -78,7 +78,7 @@ const toggleMobileNav = (whichElem) => {
                     </li>
 */
 
-let navData = [{ name: "Time-Clock", address: "https://aaronrs2002.github.io/time-clock/" }, { name: "Task Master", address: "https://aaronrs2002.github.io/task-master/" }, { name: "Analyze bank records", address: "https://aaronrs2002.github.io/analyze-bank-files/" }, { name: "Invoice Builder", address: "https://aaronrs2002.github.io/invoice-builder/" }];
+let navData = [{ name: "Task Master", address: "https://aaronrs2002.github.io/task-master/" }, { name: "Time-Clock", address: "https://aaronrs2002.github.io/time-clock/" }, { name: "Budget", address: "https://aaronrs2002.github.io/budget-app/" }, { name: "Analyze bank records", address: "https://aaronrs2002.github.io/analyze-bank-files/" }, { name: "Invoice Builder", address: "https://aaronrs2002.github.io/invoice-builder/" }];
 let navLinkHTML = "";
 for (let i = 0; i < navData.length; i++) {
     let active = "";
