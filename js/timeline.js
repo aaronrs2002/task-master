f6e24d3ba0564bc2d61e0881707e0e908374a781
Aaron Smith <aaron@web-presence.biz>
2025-02-07 08:15:05 -0700

    select random colors for timeline

--- js/timeline.js
+++ js/timeline.js
@@ -21,7 +21,7 @@ var options = {
 
 
 const runTimeline = () => {
-
+    let colorList = ["#008FFB", "#775DD0", "#FEB019", "#FF4560", "#50E2F2", "#262118"];
 
     if (localStorage.getItem("taskList")) {
         document.querySelector("#chart").innerHTML = "<div class='loader'></div>";
@@ -30,19 +30,13 @@ const runTimeline = () => {
         for (let i = 0; i < tempTaskList.length; i++) {
             if (tempNames.indexOf(tempTaskList[i].task) === -1) {
                 let tempEndYr = tempTaskList[i].details.substring(tempTaskList[i].details.length - 4, tempTaskList[i].details.length);
-
                 let tempEndDy = tempTaskList[i].details.substring(tempTaskList[i].details.indexOf(":") + 4, tempTaskList[i].details.indexOf(":") + 6);
-
                 let tempEndMo = tempTaskList[i].details.substring(tempTaskList[i].details.indexOf(":") + 1, tempTaskList[i].details.indexOf(":") + 3);
-
                 let tempEndStr = tempEndYr + "-" + tempEndMo + "-" + tempEndDy;
-                console.log("tempEndStr: " + tempEndStr);
-                console.log("tempEndYr: " + tempEndYr);
-                console.log("tempEndMo: " + tempEndMo);
-                console.log("tempEndDy: " + tempEndDy);
                 options.series[0].data = [...options.series[0].data, {//'2019-03-12'
                     x: tempTaskList[i].task, y: [new Date(timeStamp()).getTime(),
-                    new Date(tempEndStr).getTime()]
+                    new Date(tempEndStr).getTime()],
+                    fillColor: colorList[Math.floor(Math.random() * colorList.length)]
                 }];
                 document.querySelector("#chart").innerHTML = "";
 
